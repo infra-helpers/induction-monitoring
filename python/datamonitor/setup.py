b0f6848f5372b0991d1d6d7432f893ffc4d80667
michalmendygral <44322173+michalmendygral@users.noreply.github.com>
2020-07-07 09:22:39 +0200

    Update setup.py

--- python/datamonitor/setup.py
+++ python/datamonitor/setup.py
@@ -72,6 +72,7 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
         'elasticsearch',
+        'bz2'
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
