43c726b6d6f1b5eef935fbbed062fc7141c600a2
michalmendygral <44322173+michalmendygral@users.noreply.github.com>
2020-07-10 13:38:36 +0200

    Update test_datamonitor.py

--- python/datamonitor/tests/test_datamonitor.py
+++ python/datamonitor/tests/test_datamonitor.py
@@ -3,6 +3,7 @@ from datamonitor.datamonitor import DataMonitor
 from elasticmock import elasticmock
 
 class DataMonitoringTest(TestCase):
+    
     @elasticmock
     def test_should_connect_and_send_object(self):
         # Create an empty Datamonitor class instance
@@ -16,9 +17,9 @@ class DataMonitoringTest(TestCase):
         print(f"DataMonitor class instance: {dm}")
     
         # Send some simple payload to ES
-        #meta_data = {'unit': 'nb_of_rows', 'value': 6543}
-        #es_index = 'dm-test-v0'
-        #res = dm.es_send(es_index, meta_data)
+        meta_data = {'unit': 'nb_of_rows', 'value': 6543}
+        es_index = 'dm-test-v0'
+        dm.es_send(es_index, meta_data)
         #self.assertIsNotNone(res)
         
         # Debug
