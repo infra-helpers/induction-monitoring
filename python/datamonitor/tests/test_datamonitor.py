c93185adf80331242c61bc05e5f30e0e2ba54507
michalmendygral <44322173+michalmendygral@users.noreply.github.com>
2020-07-10 10:25:09 +0200

    Update test_datamonitor.py

--- python/datamonitor/tests/test_datamonitor.py
+++ python/datamonitor/tests/test_datamonitor.py
@@ -1,25 +1,25 @@
-#from unittest import TestCase
+from unittest import TestCase
 from datamonitor.datamonitor import DataMonitor
 from elasticmock import elasticmock
 
+class DataMonitoringTest(TestCase):
+    @elasticmock
+    def should_connect_and_send_object(self):
+        # Create an empty Datamonitor class instance
+        dm = DataMonitor()
 
-@elasticmock
-def test_main():
-
-    # Create an empty Datamonitor class instance
-    dm = DataMonitor()
-
-    # Connect to Elasticsearch (ES)
-    es_conn = {'host': 'localhost', 'port': 9200, 'scheme': 'http'}
-    dm.es_connect(es_conn)
-
-    # Debug
-    print(f"DataMonitor class instance: {dm}")
-
-    # Send some simple payload to ES
-    meta_data = {'unit': 'nb_of_rows', 'value': 6543}
-    es_index = 'dm-test-v0'
-    res = dm.es_send(es_index, meta_data)
-
-    # Debug
-    print(f"Result of sending to ES: {res}")
+        # Connect to Elasticsearch (ES)
+        es_conn = {'host': 'localhost', 'port': 9200, 'scheme': 'http'}
+        dm.es_connect(es_conn)
+        
+        # Debug
+        print(f"DataMonitor class instance: {dm}")
+    
+        # Send some simple payload to ES
+        meta_data = {'unit': 'nb_of_rows', 'value': 6543}
+        es_index = 'dm-test-v0'
+        res = dm.es_send(es_index, meta_data)
+        self.assertIsNotNone(res)
+        
+        # Debug
+        print(f"Result of sending to ES: {res}")
