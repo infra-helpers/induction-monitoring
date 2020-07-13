6d3585c976e5a32bff5e070439614488ac1bec36
michalmendygral <44322173+michalmendygral@users.noreply.github.com>
2020-07-13 13:28:01 +0200

    Update test_datamonitor.py

--- python/datamonitor/tests/test_datamonitor.py
+++ python/datamonitor/tests/test_datamonitor.py
@@ -24,7 +24,7 @@ class  DataMonitoringTest(TestCase):
         print(f"Result of sending to ES: {res}")
         
         # Send some simple payload to ES
-        res_2 = dm.es_send(es_index, meta_data)
+        id = dm.es_conn.index(es_index, '_doc', meta_data)
         
         # Debug
-        print(f"Result of sending to ES: {res_2}")
+        print(f"Result of sending to ES: {id}")
