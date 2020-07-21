09f3cfd996dbdc72a6c329ffc2e47cccc3165dd7
michalmendygral <44322173+michalmendygral@users.noreply.github.com>
2020-07-21 11:48:06 +0200

    Update __init__.py

--- python/datamonitor/datamonitor/__init__.py
+++ python/datamonitor/datamonitor/__init__.py
@@ -2,6 +2,6 @@
 # Source: https://github.com/infra-helpers/induction-monitoring/blob/master/python/datamonitor/datamonitor/__init__.py
 #
 
-__version__ = '0.0.4.post2'
+__version__ = '0.0.4.post3'
 
 from .datamonitor import DataMonitor
